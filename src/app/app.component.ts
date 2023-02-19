a2e0354a148a9360d874cbaef2d28fdc9fcc966b
Pilar Muñoz <86972128+pibmunoz@users.noreply.github.com>
2023-02-19 01:21:36 -0300

    Basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'timer-app';
+  title = 'Timer 20-20-20';
 }
